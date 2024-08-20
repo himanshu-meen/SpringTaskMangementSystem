f2e59a809ab229124dae910e1b18fc49242d6830
unknown <himanshumeena052599@gmail.com>
2024-08-20 23:44:36 +0530

    First Commit

---
+++ src/main/java/com/example/Task_Management/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.Task_Management;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TaskManagementApplication.class);
+	}
+
+}
